382a16994118ec925a013f372accfad3ebf1a664
laurasc14 <lsolecatala@gmail.com>
2022-10-02 19:03:43 +0200

    test PR0Array add. inicialitzate array

--- src/main/java/edu/uoc/ds/adt/PR0Array.java
+++ src/main/java/edu/uoc/ds/adt/PR0Array.java
@@ -1,4 +1,13 @@
 package edu.uoc.ds.adt;
 
+import edu.uoc.ds.adt.sequential.Queue;
+import edu.uoc.ds.adt.sequential.QueueArrayImpl;
+
+import java.lang.reflect.Array;
+
 public class PR0Array {
+
+    private int[] array = new int[50];
+
+
 }
